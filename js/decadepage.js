1fd52530c298d8b95b20b99b3ec64371450b6e66
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2019-01-30 10:27:11 +1100

    responsive decade page

--- js/decadepage.js
+++ js/decadepage.js
@@ -20,47 +20,52 @@ fetch('https://raw.githubusercontent.com/ryncmrfrd/sunderland/master/csv/players
             }
         });
     })
-//home screen navigation
-function HomeScreenNavigator(x){
-    var xid =  $(x).attr('id');
-    $('.sdl-nav-active').removeClass('sdl-nav-active');
-    $('.sdl-main-active').removeClass('sdl-main-active');
-    $('.'+ xid).addClass('sdl-main-active');
-    $(x).addClass('sdl-nav-active');
-    $('#sdl-nav-check').prop('checked', false);
-}
-//sorting numbers
-function sortNumber(a,b) { return b - a; }
-//add players into that tab
-function PlayersTab(x){
-    /*var completedplayers = [];
-    for(i = 0; i < x.length-1; i++){
-        var href = $(location).attr('href');
-        var decade = href.substring( href.lastIndexOf('/')+1 ).substring( 0,4 );
-        if(x.filter(data => data.Name === x[i].Name)[0].Decade == decade && completedplayers.indexOf(x[i].Name) === -1 ){
-            document.getElementsByClassName('players')[0].innerHTML += 
-            '<button onclick="Click('+x[i].ID+')" id="decadecard" class="sdl-home-half">'+
-                '<h2 class="sdl-welcome-card-title" style="margin-top: 5px;">'+
-                    x[i].Name+
-                    '<span class="sdl-flag sdl-flag-'+x[i].Nationality+'"></span>'+
-                '</h1>'+
-                '<div id="decadecard-image" class="sdl-home-top" style="box-shadow: none; background: url('+x[i].Picture+') center / cover;">'+
-                '</div>'+
-            '</button>';
-        completedplayers.push(x[i].Name)
-    }*/
-    var topscorers = []
-    for(i = 0; i < x.length-1; i++){ topscorers.push(x[i].Goals); }
-    topscorers = topscorers.slice(0, 4);
-    for(i = 0; i < topscorers.length; i++){ 
-        var buttonid = Number(i)+1;
-        document.querySelector('.sdl-home-players-flex').innerHTML +=
-        '<button id="top-scorer-'+buttonid+'" onclick="Click(1)">'+
-            '<div class="sdl-card sdl-card-picture" style="height:100px; width: 100px; float: left; box-shadow: none; background: url('+x[i].Picture+') center / cover;"></div>'+
-            '<h1 class="sdl-welcome-card-title">'+x[i].Name+'</h1>'+
-            '<span class="sdl-label">'+x[i].Appearances+' games, '+x[i].Goals+' goals.</span>'+
-        '</button>';
+//when mobile navigation page active allow/deny scrolling
+$('#mobileCheck').change(function(){
+    const nav = $('nav');
+    const body = $('body');
+    if($(this).is(':checked')&&nav.hasClass('active')==!1){
+        //if the checkbox has changed to the checked state
+        $(window).scrollTop(0);
+        nav.addClass('active');
+        body.addClass('navActive').bind('touchmove',function(e){e.preventDefault()})
     }
+    else{
+        nav.removeClass('active');
+        body.removeClass('navActive').unbind('touchmove')
+    }
+});
+//when navigation button is clicked, call a change tabs function and remove funky red triangle
+$('nav a.clickable').on('click', function(){
+    const thisButton = this;
+    const triangleCheck = $('#homePageTriangleCheck');
+    //remove triangle if it exists
+    if(this.id!='home'){
+        setTimeout(function(){
+            $('header').removeClass('headerHome');
+            changeTabs(thisButton)
+        },250);
+    }
+    else{
+        changeTabs(thisButton);
+    }
+    if($(window).width() <= 838 ){
+        //auto exit the navbar (mobile)
+        $('body').removeClass('navActive');
+        setTimeout(function(){
+            $('#mobileCheck').prop("checked", false);
+        },150);
+    }
+});
+//actually do the tab changing
+function changeTabs(x){
+    $('nav a.active').removeClass('active');
+    $('section.active').removeClass('active');
+    $('section#'+x.id).addClass('active');
+    $(x).addClass('active');
+    $('#navCheck').prop('checked', false);
+    $('nav').removeClass('active');
+    $('body').unbind('touchmove');
 }
 //c'est cool outline around the search bar on focus. now with fades
 $('#searchInput').focus(function(){$('.search').addClass('focus');});
@@ -81,7 +86,7 @@ function Search(parsedcsv){
             console.log(fuzzyresult)
             $('.searchResults').html(fuzzyresult);
             console.log($('.searchResults').html())
-            $('.searchResults').attr('href','../player.html?ID='+parsedcsv.filter(data => data.Name === fuzzyresult)[0].ID);
+            $('.searchResults').attr('href','../player?ID='+parsedcsv.filter(data => data.Name === fuzzyresult)[0].ID);
         }
         else{$('.searchResults').html('');}
     });
