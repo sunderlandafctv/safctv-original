fdac36e18b54e81fbee23b21dfa3d0fed9041438
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2019-02-14 12:49:05 +1100

    nicer looking

--- js/playerpage.js
+++ js/playerpage.js
@@ -119,7 +119,7 @@ function Search(parsedcsv){
 //checked videos
 function checkVideos(){
     if(loadedVideos == false){
-        $('#ifVideos').html('No videos availible. <b class="accent  ">Sorry about that.</b>');
+        $('#ifVideos').html('No videos availible. <b class="accent">Sorry about that.</b>').css('color','grey');
         $('footer').css({'position':'absolute','bottom':'0'});
     }
 }
