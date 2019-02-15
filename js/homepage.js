dbd0dbcc3cecca93fc5586853ba94c19010ceab9
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2019-02-15 20:17:52 +1100

    html formatting

--- js/homepage.js
+++ js/homepage.js
@@ -67,7 +67,7 @@ function changeTabs(x){
 */
 //go to the correct player page when clicking a player button
 function changePagePlayer(query){
-    if(query != null){window.location.href = 'player.html?ID=' + query;}
+    if(query != null){window.location.href = 'player?ID=' + query;}
 }
 //go to the correct player page
 function decadePage(filename){
